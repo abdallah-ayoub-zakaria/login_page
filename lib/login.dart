ee5d192169392a5b3ba7401a8033ad2afb968874
abdallah-ayoub-zakaria <abdallah1995333@gmail.com>
2023-09-16 16:14:37 +0300

    first commit

--- lib/login.dart
+++ lib/login.dart
@@ -1,207 +1,120 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                colors: [Colors.orange, Color(0xFFFFFFFF)])),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              child: Stack(
                 children: [
-                  Text(
-                    'Login',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold),
+                  Container(
+                    width: double.infinity,
+                    child: Positioned(
+                        child: Image.asset(
+                      'assets/images/background.png',
+                      fit: BoxFit.fill,
+                    )),
                   ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    'Welcome Back',
-                    style: TextStyle(
-                      color: Colors.white60,
-                      fontSize: 17,
-                    ),
-                  ),
+                  Positioned(
+                      left: 20,
+                      child: Image.asset('assets/images/light-1.png')),
+                  Positioned(
+                      left: 150,
+                      child: Image.asset('assets/images/light-2.png')),
+                  Positioned(
+                      top: 30,
+                      left: 350,
+                      child: Image.asset('assets/images/clock.png')),
+                  Positioned(
+                      top: 190,
+                      left: 200,
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 38,
+                            fontWeight: FontWeight.bold),
+                      )),
                 ],
               ),
             ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
-              height: 460,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius:
-                      BorderRadius.vertical(top: Radius.circular(50))),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(left: 10, right: 10),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(13),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Color.fromRGBO(225, 95, 27, 3),
-                              offset: Offset(0, 10),
-                              blurRadius: 20)
-                        ]),
-                    child: Column(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(13),
-                          ),
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              hintStyle: TextStyle(color: Colors.grey),
-                              hintText: 'Enter your UserName..',
-                            ),
-                          ),
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(13),
-                          ),
-                          child: TextFormField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                hintStyle: TextStyle(color: Colors.grey),
-                                hintText: 'Enter your Password..',
-                                border: InputBorder.none),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'Forgot Password?',
-                    style: TextStyle(color: Colors.grey, fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    alignment: Alignment.center,
-                    width: 260,
-                    height: 50,
-                    decoration: BoxDecoration(
-                        color: Color.fromRGBO(225, 95, 27, 3),
-                        borderRadius: BorderRadius.circular(33)),
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'Continue with social media',
-                    style: TextStyle(color: Colors.grey, fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ),
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.only(left: 50, right: 50),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 20,
+                            offset: Offset(0, 10),
+                            color: Colors.blue)
+                      ]),
+                  child: Column(
                     children: [
-                      Container(
-                        alignment: Alignment.center,
-                        width: 200,
-                        height: 50,
-                        decoration: BoxDecoration(
-                            color: Colors.blueAccent,
-                            borderRadius: BorderRadius.circular(33)),
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              width: 40,
-                            ),
-                            Icon(
-                              Icons.facebook,
-                              color: Colors.white,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              'Facebook',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14),
-                            ),
-                          ],
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 10),
+                        child: TextFormField(
+                          decoration:
+                              InputDecoration(hintText: 'Enter your UserName'),
                         ),
                       ),
-                      Container(
-                        alignment: Alignment.center,
-                        width: 200,
-                        height: 50,
-                        decoration: BoxDecoration(
-                            color: Colors.black,
-                            borderRadius: BorderRadius.circular(33)),
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              width: 40,
-                            ),
-                            Container(
-                              width: 25,
-                              height: 25,
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: AssetImage('assets/github.png'))),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              'Github',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14),
-                            ),
-                          ],
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: TextFormField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Enter your Password'),
                         ),
                       ),
                     ],
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Forgot Password?',
+                  style: TextStyle(color: Colors.blue, fontSize: 23),
+                )
+              ],
+            ),
+          ))
+        ],
       ),
     );
   }
